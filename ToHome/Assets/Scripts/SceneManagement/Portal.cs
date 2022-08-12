0dfc414490bc300fee3b0ff5648398e996070215
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-08-12 12:57:48 +0200

    Portal spawn point added.

--- ToHome/Assets/Scripts/SceneManagement/Portal.cs
+++ ToHome/Assets/Scripts/SceneManagement/Portal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace SceneManagement
@@ -7,6 +8,7 @@ namespace SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] private int destinationSceneIndex = -1;
+        [SerializeField] private Transform spawnPoint;
         
         private void OnTriggerEnter(Collider other)
         {
@@ -19,8 +21,31 @@ namespace SceneManagement
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(destinationSceneIndex);
+            
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
             print("Scene loaded.");
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (var portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+
+            return null;
         }
     }
 }
