a1909c5fab8102ceabe0bcf93688904db40399ff
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-08-13 10:32:44 +0200

    Serializing with use of BinaryFormatter.

--- ToHome/Assets/Scripts/Saving/SavingSystem.cs
+++ ToHome/Assets/Scripts/Saving/SavingSystem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -14,8 +15,9 @@ namespace Saving
             using (FileStream stream = File.Open(path, FileMode.Create))
             {
                 Transform playerTransform = GetPlayerTransform();
-                byte[] buffer = SerializeVector(playerTransform.position);
-                stream.Write(buffer, 0, buffer.Length);
+                var binaryFormatter = new BinaryFormatter();
+                var position = new SerializableVector3(playerTransform.position);
+                binaryFormatter.Serialize(stream, position);
             }
         }
 
@@ -25,14 +27,14 @@ namespace Saving
             print("Loading from " + path);
             using (FileStream stream = File.Open(path, FileMode.Open))
             {
-                byte[] buffer = new byte[stream.Length];
-                stream.Read(buffer, 0, buffer.Length);
-
+                var binaryFormatter = new BinaryFormatter();
+                var position = (SerializableVector3) binaryFormatter.Deserialize(stream);
+                
                 var navMeshAgent = GameObject.FindWithTag("Player").GetComponent<NavMeshAgent>();
                 navMeshAgent.enabled = false;
                 
                 Transform playerTransform = GetPlayerTransform();
-                playerTransform.position = DeserializeVector(buffer);
+                playerTransform.position = position.ToVector();
                 
                 navMeshAgent.enabled = true;
 
@@ -47,26 +49,6 @@ namespace Saving
         private Transform GetPlayerTransform()
         {
             return GameObject.FindWithTag("Player").transform;
-        }
-
-        private byte[] SerializeVector(Vector3 vector)
-        {
-            var vectorBytes = new byte[3 * 4];
-            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
-            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
-            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
-            
-            return vectorBytes;
-        }
-        
-        private Vector3 DeserializeVector(byte[] buffer)
-        {
-            var vector = new Vector3();
-            vector.x = BitConverter.ToSingle(buffer, 0);
-            vector.y = BitConverter.ToSingle(buffer, 4);
-            vector.z = BitConverter.ToSingle(buffer, 8);
-
-            return vector;
         }
     }
 }
---
+++ ToHome/Assets/Scripts/Saving/SerializableVector3.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+namespace Saving
+{
+    [Serializable]
+    public class SerializableVector3
+    {
+        private float _x, _y, _z;
+        
+        public SerializableVector3(Vector3 vector)
+        {
+            _x = vector.x;
+            _y = vector.y;
+            _z = vector.z;
+        }
+
+        public Vector3 ToVector()
+        {
+            return new Vector3(_x, _y, _z);
+        }
+    }
+}
