04a5764b0b1edff29657b4856fbe21dc3c013532
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-08-21 12:24:44 +0200

    Pickup respawning feature added.

--- ToHome/Assets/Scripts/Combat/WeaponPickup.cs
+++ ToHome/Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Combat
@@ -5,13 +6,30 @@ namespace Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] private Weapon weaponSO;
+        [SerializeField] private float respawnTime = 5f;
 
         private void OnTriggerEnter(Collider other)
         {
             if (!other.tag.Equals("Player")) return;
             
             other.GetComponent<Fighter>().EquipWeapon(weaponSO);
-            Destroy(gameObject);
+            StartCoroutine(HideForSeconds(respawnTime));
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            SetPickupVisibility(false);
+            yield return new WaitForSecondsRealtime(seconds);
+            SetPickupVisibility(true);
+        }
+
+        private void SetPickupVisibility(bool shouldShow)
+        {
+            GetComponent<Collider>().enabled = shouldShow;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
+            }
         }
     }
 }
