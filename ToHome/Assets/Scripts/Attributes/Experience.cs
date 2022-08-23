2b279e900e8401b9a15940522c43c3a7f6fdf5ac
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-08-23 11:17:32 +0200

    Feature of saving experience points added.

--- ToHome/Assets/Scripts/Attributes/Experience.cs
+++ ToHome/Assets/Scripts/Attributes/Experience.cs
@@ -1,14 +1,25 @@
+using Saving;
 using UnityEngine;
 
 namespace Attributes
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, ISavable
     {
         [SerializeField] private float experience;
 
         public void GainExperience(float experiencePoints)
         {
             experience += experiencePoints;
+        }
+
+        public object CaptureState()
+        {
+            return experience;
+        }
+
+        public void RestoreState(object state)
+        {
+            experience = (float) state;
         }
     }
 }
